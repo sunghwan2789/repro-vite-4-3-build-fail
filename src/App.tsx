6994b6ba96c50b90d4a188b6bde359962c804d77
방성환 <sunghwan@daimresearch.com>
2023-04-21 08:46:10 +0900

    repro

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,18 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Accordion, AccordionSummary, Typography } from '@mui/material'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <Accordion>
+        <AccordionSummary>
+          <Typography>title</Typography>
+        </AccordionSummary>
+      </Accordion>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
